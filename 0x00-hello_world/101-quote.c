6dd0c8c4695e913bbcfa3505bbc7620909652629
TheHorsfall <horsfallsobomaboa.com>
2022-07-02 09:54:54 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-*main - Prints all combinations of three digits with,
+*main - and that piece of art is useful" - Dora Korpar, 2015-10-19
 * and space followed by new line
 *
-*Return: returns 0
+*Return: returns 1
 */
 int main(void)
 {
