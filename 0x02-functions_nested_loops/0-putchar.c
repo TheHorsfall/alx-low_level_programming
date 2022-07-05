3059fe9385af00b1d2cd84439f094c469013d93f
TheHorsfall <horsfallsobomaboa.com>
2022-07-05 15:04:14 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * main - function to print
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char holberton[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+	char main[4] = {'M', 'A', 'I', 'N'};
 	unsigned int c;
 
-	for (c = 0; c < sizeof(holberton); c++)
+	for (c = 0; c < sizeof(main); c++)
 	{
-		_putchar(holberton[c]);
+		_putchar(main[c]);
 	}
 _putchar('\n');
 return (0);
