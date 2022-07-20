07d3466247696b6d0588edbae70b99ba3735c3ba
TheHorsfall <horsfallsobomaboa.com>
2022-07-20 15:31:35 -0700

    C Programming Day 8

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -26,4 +26,25 @@ int p1(char *s, int l)
 {
 	if (l < 1)
 	{
+		return (1);
+	}
 
+	if (*s == *(s + l))
+	{
+		return (p1(s + 1, l - 2));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - palindrome
+ * @s: pointer to string
+ * Return: recursion
+ */
+
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	return (p1(s, len - 1));
+}
